bee4a2d22999b55918d847bbe426488c25200782
liviucristianmunteanu <liviucristianmunteanu.github@gmail.com>
2022-11-23 21:34:53 +0200

    added Superhero and Pokemon entities

---
+++ src/main/java/com/itschool/superhero/entity/Pokemon.java
@@ -0,0 +1,23 @@
+package com.itschool.superhero.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Pokemon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "superhero_id")
+    private Superhero superhero;
+}
---
+++ src/main/java/com/itschool/superhero/entity/Superhero.java
@@ -0,0 +1,27 @@
+package com.itschool.superhero.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@ToString
+public class Superhero {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String superpower;
+
+    @OneToMany
+    private List<Pokemon> pokemons = new ArrayList<>();
+}
