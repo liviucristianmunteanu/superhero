6a3da8225abbbfd5394b5f3c371ba2347e26c850
liviucristianmunteanu <liviucristianmunteanu.github@gmail.com>
2022-11-27 19:47:24 +0200

    added pokemon query

--- src/main/java/com/itschool/superhero/repository/PokemonRepository.java
+++ src/main/java/com/itschool/superhero/repository/PokemonRepository.java
@@ -1,7 +1,13 @@
 package com.itschool.superhero.repository;
 
 import com.itschool.superhero.entity.Pokemon;
+import com.itschool.superhero.entity.Superhero;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PokemonRepository extends JpaRepository <Pokemon, Long> {
+    @Query("SELECT p DISTINCT FROM Pokemon p ORDER BY p.name desc")
+    List<Pokemon> findPokemonDistinctByName();
 }
