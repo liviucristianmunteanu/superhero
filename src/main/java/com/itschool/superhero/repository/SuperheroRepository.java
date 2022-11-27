8ff2608f64a97f9ab6a1e78c2bf5dab7ab91007a
liviucristianmunteanu <liviucristianmunteanu.github@gmail.com>
2022-11-27 19:48:17 +0200

    added superheroes queries

--- src/main/java/com/itschool/superhero/repository/SuperheroRepository.java
+++ src/main/java/com/itschool/superhero/repository/SuperheroRepository.java
@@ -2,6 +2,17 @@ package com.itschool.superhero.repository;
 
 import com.itschool.superhero.entity.Superhero;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface SuperheroRepository extends JpaRepository  <Superhero, Long>{
+    List<Superhero> findByNameEndingWith(String name);
+    List<Superhero> findByNameStartingWith(String name);
+
+    List<Superhero> findAllSuperheroesByIdLessThanEqual(Long id);
+    @Query("SELECT s FROM Superhero s WHERE s.name IS NULL OR s.superpower IS NULL")
+    List<Superhero> findAllSuperheroesIsNull();
 }
